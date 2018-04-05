cace71947194028c235ffef5a3f8fb1d9adba2d0
Gesiane Reis <gesiane.silva@concrete.com.br>
2018-04-05 11:03:48 -0300

    criado o arquivo index, corrigido os testes

--- test/test.js
+++ test/test.js
@@ -4,10 +4,13 @@ var request = require('supertest');
 
 var app = require('../server.js')
 
- describe('GET /', function() {
+ describe('GET /', function(done) {
    it('displays "Hello World!"', function(done) {
      // The line below is the core test of our app.
-     request(app).get('/').expect('Hello World!', done);
+     request(app).get('/')
+      .expect('Hello World!')
+      .then(() => done())
+      .catch(err => done(err));
    });
  });
   
